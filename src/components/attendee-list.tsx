214e752e0b0f9f788c6ec57596e1a93bd7c8a656
Joyce <joycesantos8900@gmail.com>
2024-04-04 13:32:32 -0300

    feito: tableHeader

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,9 +1,12 @@
 import { Search,
-ChevronLeft,
+  ChevronLeft,
   ChevronRight,
   ChevronsLeft,
   ChevronsRight,
   MoreHorizontal, } from "lucide-react"
+import { IconButton } from "./icon-button";
+import { Table } from "./table/table";
+import { TableHeader } from "./table/table-header";
 export function AttendeeList (){
    return (
     <div className="flex flex-col gap-4">
@@ -18,28 +21,27 @@ export function AttendeeList (){
         </div>
       </div>
 
-      <div className="border border-white/10 rounded-lg">
-        <table className="w-full">
+     <Table>
           <thead>
             <tr className="border-b border-white/10">
-              <th
+              <TableHeader
                 style={{ width: 48 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
+                
               >
                 <input type="checkbox" className="size-4 bg-black/20 rounded border border-white/10" />
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">Código</th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">Participante</th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
+              </TableHeader>
+              <TableHeader >Código</TableHeader>
+              <TableHeader >Participante</TableHeader>
+              <TableHeader >
                 Data de inscrição
-              </th>
-              <th className="py-3 px-4 text-sm font-semibold text-left">
+              </TableHeader>
+              <TableHeader >
                 Data do check-in
-              </th>
-              <th
+              </TableHeader>
+              <TableHeader
                 style={{ width: 64 }}
-                className="py-3 px-4 text-sm font-semibold text-left"
-              ></th>
+                
+              ></TableHeader>
             </tr>
           </thead>
           <tbody>
@@ -61,9 +63,10 @@ export function AttendeeList (){
                   <td className="py-3 px-4 text-sm text-zinc-300">7 dias atrás</td>
                   <td className="py-3 px-4 text-sm text-zinc-300">3 dias atrás</td>
                   <td className="py-3 px-4 text-sm text-zinc-300">
-                    <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
+                   
+                     <IconButton transparent={true}>
                       <MoreHorizontal className="size-4" />
-                    </button>
+                    </IconButton>
                   </td>
                 </tr>
               );
@@ -71,7 +74,7 @@ export function AttendeeList (){
           </tbody>
           <tfoot>
             <tr>
-              <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
+            <td className="py-3 px-4 text-sm text-zinc-300" colSpan={3}>
                 Mostrando 10 de 228 itens
               </td>
               <td className="py-3 px-4 text-sm text-zinc-300 text-right" colSpan={3}>
@@ -79,25 +82,24 @@ export function AttendeeList (){
                   <span>Página 1 de 23</span>
 
                   <div className="flex gap-1.5">
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                      <ChevronsLeft className="size-4" />
-                    </button>
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                      <ChevronLeft className="size-4" />
-                    </button>
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
-                      <ChevronRight className="size-4" />
-                    </button>
-                    <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                    <IconButton>
                       <ChevronsRight className="size-4" />
-                    </button>
+                    </IconButton>
+                    <IconButton>
+                      <ChevronsRight className="size-4" />
+                    </IconButton>
+                    <IconButton>
+                      <ChevronsRight className="size-4" />
+                    </IconButton>
+                    <IconButton>
+                      <ChevronsRight className="size-4" />
+                    </IconButton>
                   </div>
                 </div>
               </td>
             </tr>
           </tfoot>
-        </table>
-      </div>
+        </Table>
     </div>
   );
 } 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,13 @@
 import nlwUniteIcon from "../assets/nlw-unite-icon.svg"
+import { NavLink } from "./nav-link"
 
 export function Header(){
     return (
         <div className="flex items-center gap-5 py-2">
             <img src={nlwUniteIcon} alt="icon" />
             <nav className="flex items-center gap-5">
-                <a href="" className="font-mediun text-sm text-zinc-300">Eventos</a>
-                <a href="" className="font-mediun text-sm">Participantes</a>
+              <NavLink href="/eventos">Eventos</NavLink>  
+              <NavLink href="/participantes">Participantes</NavLink>
             </nav>
         </div>
     )
